c4f837c6f4912a655936ed4f1c375fc702981f19
Petter Karlsrud <petterkarlsrud@me.com>
2021-03-23 16:38:34 +0100

    Store the body and header as JSON

--- src/http.rs
+++ src/http.rs
@@ -1,34 +1,57 @@
+use std::collections::HashMap;
 use std::io::Cursor;
 use rocket::Response;
 use url::Url;
 use rocket::{Request, Data};
 use rocket::handler::{Handler, Outcome};
-use rocket::http::ContentType;
+use serde_json::json;
+use serde::{Deserialize, Serialize};
+
 
 type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct StorableResponse {
+    pub body: String,
+    pub headers: HashMap<String, String>,
+}
+
 pub async fn get_json(url: &String) -> Result<serde_json::Value> {
     let url = Url::parse(&url)?;
+    let resp = reqwest::get(url).await?;
 
-    let resp = reqwest::get(url)
-        .await?
+    let mut headers: HashMap<String, String> = HashMap::new();
+    for (key, value) in resp.headers().iter() {
+        let data = value.to_str()?;
+        headers.insert(key.to_string(), data.to_string());
+    }
+
+    let data = resp
         .json::<serde_json::Value>()
         .await?;
+    
+    let body = data.to_string();
+    let storable_response = StorableResponse { body, headers };
 
-    return Ok(resp);
+    return Ok(json!(storable_response));
 }
 
 #[derive(Clone)]
 pub struct JSONHandler{ 
-    pub data: String 
+    pub body: String,
+    pub headers: HashMap<String, String>
 }
 
 impl Handler for JSONHandler {
     fn handle<'r>(&self, req: &'r Request, _data: Data) -> Outcome<'r> {
         let mut response = Response::new();
-        response.set_header(ContentType::JSON);
-        response.set_sized_body(Cursor::new(self.data.clone()));
+
+        for (key, value) in self.headers.iter() {
+            response.adjoin_raw_header(key.clone(), value.clone());
+        }
+
+        response.set_sized_body(Cursor::new(self.body.clone()));
         return Outcome::from(req, response);
     }
 }
